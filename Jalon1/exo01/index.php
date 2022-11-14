feaad1bc67e868833f4496614dc6180029ab78d4
Fabien Plouvier <fabien.plouvier@etu.imt-nord-europe.fr>
2022-11-14 13:09:06 +0100

    correction mineur

--- Jalon1/exo01/index.php
+++ Jalon1/exo01/index.php
@@ -8,7 +8,7 @@
     <h1>Jalon : 1</h1>
     <h2>Exo : 1</h2>
 
-    <p>Sujet : On affiche l''heure courante</p>
+    <p>Sujet : On affiche l'heure courante</p>
 
     <?php
     echo "On est le " , date('d/m/y'), " et il est ", date("H:i:s");
