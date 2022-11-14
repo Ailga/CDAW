9c8080d10b445a5f20cdff1081109d073cbfb2f2
Ailga <101090659+Ailga@users.noreply.github.com>
2022-11-14 17:28:43 +0100

    Update Premiers-exercices.php

--- TPs/TPs Gaëlle/Premiers-exercices.php
+++ TPs/TPs Gaëlle/Premiers-exercices.php
@@ -1,4 +1,26 @@
 <?php
 echo 'Hello World <br>';
+
+function distance(string $strandA, string $strandB): int
+{
+    $lenA = strlen($strandA);
+    $lenB = strlen($strandB);
+    if ($lenA == $lenB){
+        $Hamming =0; 
+        for ($i = 0; $i <= $lenA-1; $i++) {
+            if ($strandA[$i] != $strandB[$i]){
+                echo $strandA[$i].$strandB[$i];
+                $Hamming += 1;
+            }
+        return $Hamming;
+        }
+    }else{
+        throw new \InvalidArgumentException("DNA strands must be of equal length.");
+    }
+    
+}
+
+
+echo distance('GCCTA','AGAGA');
 ?>
 
