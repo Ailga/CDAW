c1bcf80411cc01bac818c2d73d197040296e73ff
Fabien Plouvier <fabien.plouvier@etu.imt-nord-europe.fr>
2022-11-14 11:16:10 +0100

    jalon 1 - exo 1 - OK

---
+++ Jalon1/exo01/index.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title>UV-CDAW</title>
+    <meta charset="utf-8">
+  </head>
+
+  <body>
+    <h1>Jalon : 1</h1>
+    <h2>Exo : 1</h2>
+
+    <p>Sujet : On affiche l''heure courante</p>
+
+    <?php
+    echo "On est le " , date('d/m/y'), " et il est ", date("H:i:s");
+    ?>
+
+  </body>
+
+<?php
+?>
