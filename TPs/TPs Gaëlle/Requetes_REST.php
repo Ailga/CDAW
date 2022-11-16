37a464cd279ae472ad452bb5b9410d4b41be2628
Ailga <101090659+Ailga@users.noreply.github.com>
2022-11-16 11:23:41 +0100

    Tableau de pokemons

---
+++ TPs/TPs Gaëlle/Requetes_REST.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title> Liste de Pokemons</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1> Pokemons</h1>
+
+<table>
+    <tbody>
+<?php
+for ($i = 1; $i <= 200; $i++){
+    $json = file_get_contents('https://pokeapi.co/api/v2/pokemon/'.$i);
+    $info=json_decode($json,true);
+    echo "<tr><td>".$info['id']."</td>";
+    $urlimage=$info["sprites"]["other"]["official-artwork"]["front_default"];
+    echo "<td><img src='$urlimage', width=50px /></td>";
+    echo "<td>".$info['name']."</td></tr>";
+}
+?>
+    </tbody>
+</table>
