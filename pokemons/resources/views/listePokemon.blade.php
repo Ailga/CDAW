369287fdf6854b9da16b22b8e76d12fa64d6c7b6
Ailga <101090659+Ailga@users.noreply.github.com>
2022-11-17 11:14:57 +0100

    Debuts de Laravel

---
+++ pokemons/resources/views/listePokemon.blade.php
@@ -0,0 +1,7 @@
+<!doctype html>
+    <html lang="{{ app()->getLocale() }}">
+        <?php
+            echo "Ca fontionne?"
+        ?>
+    </html>
+    
---
+++ pokemons/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/{prenom}/{nom}', function ($prenom, $nom) {
+    $listePokemon="Liste des Pokemons";
+    echo $prenom.$listePokemon;
+    //return "Hello World";
+    /*return view('welcome');*/
+
+})->where('prenom','[A-Za-z]+')->where('nom','[A-Za-z]+');
