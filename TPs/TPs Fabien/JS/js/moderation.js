de0c5a2e84082e2402ba128213ce9459e4d945f0
Fabien Plouvier <fabienplouvier5@gmail.com>
2022-11-23 10:49:38 +0100

    js 3/5 en cours

--- TPs/TPs Fabien/JS/js/moderation.js
+++ TPs/TPs Fabien/JS/js/moderation.js
@@ -3,28 +3,31 @@
 $(document).ready(function () {
     let indexNewComment = 4;        //Car on a deja la balise user3
     function modify(e) {
-        let idElement = e.currentTarget.parentNode.id;
+        var idElement = e.currentTarget.parentNode.id;
         alert(e.type + " on modify for " + idElement + " !");
-        let element = document.getElementById(idElement);
-        element.children[1].innerHTML = "Chaîne modifiée!!";
+        let elementModifier = document.getElementById(idElement);
+        //element.children[1].innerHTML = "Chaîne modifiée!!";
+        // On met à jour le textarea
+        $('#myText').val(elementModifier.children[1].innerHTML);
+
     }
 
     function deleter(e) {
-        let idElement = e.currentTarget.parentNode.id;
+        var idElement = e.currentTarget.parentNode.id;
         alert(e.type + " on remove for " + idElement + " !");
-        let element = document.getElementById(idElement);
+        var element = document.getElementById(idElement);
         element.remove();
     }
 
     document.getElementById("addNew").addEventListener("click", function (e) {
         alert(e.type + " on add !");
-        let divUsers = document.getElementById("users");
-        let divNewUsers = document.createElement("div");
-        let newBtnModify = document.createElement("button");
+        var divUsers = document.getElementById("users");
+        var divNewUsers = document.createElement("div");
+        var newBtnModify = document.createElement("button");
         newBtnModify.classList.add("modify");
         newBtnModify.innerText = "Modify Comment";
 
-        let newBtnRemove = document.createElement("button");
+        var newBtnRemove = document.createElement("button");
         newBtnRemove.classList.add("remove");
         newBtnRemove.innerText = "Remove Comment";
 
@@ -39,9 +42,26 @@ $(document).ready(function () {
         newBtnRemove.addEventListener("click", deleter);
     })
 
+
+    function checkNewCommentValide(event){
+        alert("test check call et elementModifier = " + elementModifier);
+        var valToCheck = event.currentTarget.elements[0].value; //récupère la valeur du texte dans le premier input
+        if(valToCheck == ""){
+            alert("ERREUR - Saisie incorrecte : Le nouveau commentaire ne peut pas être nul");
+            event.preventDefault();
+        }else{
+            alert("SUCCES - Saisie valide : Le commentaire de " + elementModifier + " va être modifié");
+            $('#' + elementModifier).val(valToCheck);
+        }
+    }
+
+
+
     let modifiers = document.getElementsByClassName("modify");
     Array.from(modifiers).forEach(m => m.addEventListener("click", modify));
 
     let remover = document.getElementsByClassName("remove");
     Array.from(remover).forEach(m => m.addEventListener("click", deleter));
+
+    document.forms["myForm"].addEventListener("submit", checkNewCommentValide);
 });
