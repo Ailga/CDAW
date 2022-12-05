1a2e8a9aa02d7c0dab1048c9b84e061347ede3ce
Ailga <101090659+Ailga@users.noreply.github.com>
2022-12-05 12:08:03 +0100

    Lundi 05/12

--- pokemon/database/seeders/PokemonSeeder.php
+++ pokemon/database/seeders/PokemonSeeder.php
@@ -16,11 +16,11 @@ class PokemonSeeder extends Seeder
     public function run()
     {
 
-        function getPokemonFromAPI($url, $finurl, $numberPokemon)
+        function getPokemonFromAPI($url, $limiteUrl, $numberPokemon)
         {
             //On récupère la liste des pokemons (qui contient que le nom et le lien des détails)
             $pathImg = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"; // + 1.png par exemple
-            $getListPokemon = file_get_contents($url.$finurl);
+            $getListPokemon = file_get_contents($url . $limiteUrl);
             if($getListPokemon !=false){
                 $listPokemonObj = json_decode($getListPokemon);
                 $resultsPokemon = $listPokemonObj->{'results'};
@@ -28,12 +28,37 @@ class PokemonSeeder extends Seeder
                 {
                     $pokemon = $resultsPokemon[$index - 1];
                     $imgPokemon = $pathImg . $index . ".png";
-                    list($energyPokemon, $pv_max, $weight, $height) = getInfoFromPokemon($url ."/". strval($index));
-                    createTableFromAPI($pokemon->{'name'}, $energyPokemon, $pv_max, 0, $weight, $height, $imgPokemon);
+                    list($energyPokemon, $pv_max, $weight, $height, $scoreAttaqueNormale, $scoreAttaqueSpeciale, $scoreDefenseSpeciale) = getInfoFromPokemon($url . "/" . strval($index));
+                    createTableFromAPI($pokemon->{'name'}, $energyPokemon, $pv_max, 0, $weight, $height, $scoreAttaqueNormale, $scoreAttaqueSpeciale, $scoreDefenseSpeciale, $imgPokemon);
+                    echo "Debug : Pokemon n°" . $index . "/$numberPokemon créé ✅\n";
                 }
             }else{
                 echo "Erreur : Impossible d'avoir la liste des pokemons";
             }
+        }
+
+        function getIdEnergy($energyPokemon){
+            $listEnergy = array(
+                "normal" => "1",
+                "fighting" => "2",
+                "flying" => "3",
+                "poison" => "4",
+                "ground" => "5",
+                "rock" => "6",
+                "bug" => "7",
+                "ghost" => "8",
+                "steel" => "9",
+                "fire" => "10",
+                "water" => "11",
+                "grass" => "12",
+                "electric" => "13",
+                "psychic" => "14",
+                "ice" => "15",
+                "dragon" => "16",
+                "dark" => "17",
+                "fairy" => "18"
+            );
+            return $listEnergy[$energyPokemon];
         }
 
         //On récupère d'autres info comme le hp ou l'energie d'un pokemon
@@ -44,24 +69,31 @@ class PokemonSeeder extends Seeder
                 $getInfosPokemonObj = json_decode($getInfosPokemon);
                 $energyPokemon = $getInfosPokemonObj->{'types'}[0]->{'type'}->{'name'};
                 $pv_max = $getInfosPokemonObj->{'stats'}[0]->{'base_stat'};
+                $scoreAttaqueNormale = $getInfosPokemonObj->{'stats'}[1]->{'base_stat'};
+                $scoreAttaqueSpeciale = $getInfosPokemonObj->{'stats'}[3]->{'base_stat'};
+                $scoreDefenseSpeciale = $getInfosPokemonObj->{'stats'}[4]->{'base_stat'};
                 $weight = $getInfosPokemonObj->{'weight'};
                 $height = $getInfosPokemonObj->{'height'};
-                return array($energyPokemon, $pv_max, $weight, $height);
+                $id_energy=getIdEnergy($energyPokemon);
+                return array($id_energy, $pv_max, $weight, $height, $scoreAttaqueNormale, $scoreAttaqueSpeciale, $scoreDefenseSpeciale);
             }else{
                 echo "Erreur : Impossible d'avoir des infos sur le pokemon";
             }
         }
 
         //On insert dans la table une nouvelle ligne
-        function createTableFromAPI($name, $energy, $pv_max, $level, $weight, $height, $pathImg)
+        function createTableFromAPI($name, $energy, $pv_max, $level, $weight, $height, $scoreAttaqueNormale, $scoreAttaqueSpeciale, $scoreDefenseSpeciale, $pathImg)
         {
             DB::table('pokemon')->insert([
                 'name' => $name,
-                'energy' => $energy,
+                'id_energie' => $energy,
                 'pv_max' => $pv_max,
                 'level' => $level,
                 'weight' => $weight,
                 'height' => $height,
+                'scoreNormalAttack' => $scoreAttaqueNormale,
+                'scoreSpecialAttack' => $scoreAttaqueSpeciale,
+                'scoreSpecialDefense' => $scoreDefenseSpeciale,
                 'pathImg' => $pathImg
                ]);
         }
--- pokemon/resources/views/listePokemon.blade.php
+++ pokemon/resources/views/listePokemon.blade.php
@@ -2,74 +2,29 @@
 @section('content')
 
 <head>
-<script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.9.1.min.js" type="text/javascript"></script>
 <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
-<link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+
 </head>
 
 <h1>Bestiaire pokemons</h1>
-<?php
 
-function CallAPI($url)
-{
-    $response = file_get_contents($url);
-    $pathImg = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"; // + 1.png par ex
-    if ($response != false)
-    {
-        $responseObj = json_decode($response);
-        $results = $responseObj->{'results'};
-        echo "<table id='myTable' class='display'>";
-        echo "<thead><tr><th>Name</th><th>Image</th></tr></thead>";
-        echo "<tbody>";
-        $index = 0;
-        foreach($results as $infoPokemon)
-        {
-            $index ++;
-            $imgSource = "<img src='$pathImg$index.png' width='90' height='90'>";
-            echo "<tr>";
-            echo "<td width=10px align='center'>" . $infoPokemon->{'name'} ."</td>";
-            echo "<td width=10px align='center'>" .$imgSource . "</td>";
+<table id='myTable' class='display'>
+<thead><tr><th>Name</th><th>Energie</th><th>Image</th></tr></thead>
+<tbody>
+@foreach($infosPokemon as $pokemon)
+<tr>
+  <td width=10px align='center'>{{$pokemon->name}}</td>
+  <td width=10px align='center'>{{$pokemon->id_energie}}</td>
+  <td width=10px align='center'>
+    <img src={{$pokemon->pathImg}} width='90' height='90'>
+  </td>
+</tr>
 
-            echo "</tr>";
-        }
-        echo "</tbody></table>";
+@endforeach
 
-    }else{
-        echo "Aucune valeur retournée";
-    }
-}
+</tbody></table>
 
-
-function buildBestiaireFromDB($queryBestiaire)
-{
-
-  echo "<table id='myTable' class='display'>";
-  echo "<thead><tr><th>Name</th><th>Energie</th><th>Image</th></tr></thead>";
-  echo "<tbody>";
-  foreach($queryBestiaire as $pokemon)
-  {
-    $urlImg = $pokemon->{'pathImg'};
-    $imgSource = "<img src='$urlImg' width='90' height='90'>";
-
-    echo "<tr>";
-    echo "<td width=10px align='center'>" . $pokemon->{'name'} ."</td>";
-    echo "<td width=10px align='center'>" . $pokemon->{'energy'} ."</td>";
-    echo "<td width=10px align='center'>" .$imgSource . "</td>";
-    echo "</tr>";
-  }
-  echo "</tbody></table>";
-}
-
-//CallAPI("https://pokeapi.co/api/v2/pokemon/")
-buildBestiaireFromDB($infosPokemon);
-
-?>
-
-
-<script>
-  $(document).ready( function () {
-    $('#myTable').DataTable();
-} );
-</script>
 
