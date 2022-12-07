309f639c8a25209379208e6ab120c95908c03000
Ailga <101090659+Ailga@users.noreply.github.com>
2022-12-07 11:59:52 +0100

    datatable

--- pokemon/resources/views/listePokemon.blade.php
+++ pokemon/resources/views/listePokemon.blade.php
@@ -5,12 +5,14 @@
 <script src="https://code.jquery.com/jquery-1.9.1.min.js" type="text/javascript"></script>
 <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="{{asset('css/datatableStyles.css')}}">
 
 
 </head>
 
-<h1>Bestiaire pokemons</h1>
+<h1>Pokédex</h1>
 
+<div class="container">
 <table id='myTable' class='display'>
 <thead><tr><th>Name</th><th>Energie</th><th>Image</th></tr></thead>
 <tbody>
@@ -25,6 +27,6 @@
 
 @endforeach
 
-</tbody></table>
+</tbody></table></div>
 
 
