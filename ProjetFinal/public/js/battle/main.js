3134c235be0aba7dfcb385b39fc3ca6fc5b5ab2c
Fabien Plouvier <fabienplouvier5@gmail.com>
2022-12-12 16:45:27 +0100

    affrontement avec btn OK - reste à faire : choisir qui commence en premier + get 3 pokemons par player

--- ProjetFinal/public/js/battle/main.js
+++ ProjetFinal/public/js/battle/main.js
@@ -8,24 +8,25 @@ let socket = io(ip_address + ':' + socket_port);
 
 function setObjPokemonPlayer(jsonPokemon){
   tourBattle.player = {};
+  player.status = "NOK";
   player.pokemon = {}
-  player.pokemon.availableNormalAttack, player.pokemon.availableSpecialAttack, player.pokemon.availableSpecialDefense = true;
   player.pokemon = jsonPokemon;
   player.pokemon.hp = player.pokemon.pv_max;
-  player.pokemon.availableNormalAttack = true;
-  player.pokemon.availableSpecialAttack = true;
-  player.pokemon.availableSpecialDefense = true;
+  player.pokemon.availableNormalAttack = false;
+  player.pokemon.availableSpecialAttack = false;
+  player.pokemon.availableSpecialDefense = false;
   tourBattle.player.pokemon = player.pokemon;
   console.log("obj pokemon player set ! pokemon : " + player.pokemon.name);
 }
 
 function setObjPokemonOpponent(jsonPokemon){
+  opponent.status = "NOK";
   opponent.pokemon = {};
   opponent.pokemon = jsonPokemon;
   opponent.pokemon.hp = opponent.pokemon.pv_max;
-  opponent.pokemon.availableNormalAttack = true;
-  opponent.pokemon.availableSpecialAttack = true; 
-  opponent.pokemon.availableSpecialDefense = true;
+  opponent.pokemon.availableNormalAttack = false;
+  opponent.pokemon.availableSpecialAttack = false; 
+  opponent.pokemon.availableSpecialDefense = false;
   console.log("obj pokemon opponent set ! pokemon : " + opponent.pokemon.name);
 }
 
@@ -34,48 +35,91 @@ function notifyMessage(whichPlayer, message){
   $("#info" + whichPlayer)[0].children.notification.innerText = message;
 }
 
+function resetStateBtnAndPlayer(){
+  player.status = "NOK";
+  opponent.status = "NOK";
+
+  $("#attaqueSpeciale").removeClass("btnClicked");
+  $("#defenseSpeciale").removeClass("btnClicked");
+  $("#attaqueNormale").removeClass("btnClicked");
+  $("#attaqueSpeciale").removeClass("btnDisabled");
+  $("#defenseSpeciale").removeClass("btnDisabled");
+  $("#attaqueNormale").removeClass("btnDisabled");
+  $("#btnContinue").removeClass("btnClicked");
+  $("#btnContinue").removeClass("btnEnabled");
+
+  if(player.pokemon.availableSpecialAttack){
+    $("#attaqueSpeciale").addClass("btnDisabled");
+  }
+  if(player.pokemon.availableSpecialDefense){
+    $("#defenseSpeciale").addClass("btnDisabled");
+  }
+  
+  $("#message")[0].innerText = "Que doit faire " + player.pokemon.name + " ?";
+  $("#infoPlayer")[0].children.statutCombat.innerText = "État : Pas prêt";
+  $("#infoPlayer #statutCombat").css("color", "red");
+  $("#infoOpponent")[0].children.statutCombat.innerText = "État : Pas prêt";
+  $("#infoOpponent #statutCombat").css("color", "red");
+
+}
 
 function chooseActionPlayerTour(whichPlayer, typeAttack){
   let actionPlayer = {};
   console.log("fonction chooseActionPlayerTour appelée avec param : " + typeAttack);
   player.pokemon.actionTour = typeAttack;
-  if(typeAttack == "attaqueNormale" && player.pokemon.availableNormalAttack){
-    player.pokemon.availableNormalAttack = true;
-    player.pokemon.availableSpecialDefense = false;
+  if(typeAttack == "attaqueNormale"){
     actionPlayer.score = player.pokemon.scoreNormalAttack;
+    $("#attaqueSpeciale").addClass("btnDisabled");
+    $("#defenseSpeciale").addClass("btnDisabled");
     //doAttack(whichPlayer, typeAttack);
-  }else if(typeAttack == "attaqueSpeciale" && player.pokemon.availableSpecialAttack){
+  }else if(typeAttack == "attaqueSpeciale" && ! player.pokemon.availableSpecialAttack){
     player.pokemon.availableSpecialAttack = true;
-    player.pokemon.availableSpecialDefense = false;
     actionPlayer.score = player.pokemon.scoreSpecialAttack;
+    $("#defenseSpeciale").addClass("btnDisabled");
+    $("#attaqueNormale").addClass("btnDisabled");
     //doAttack(whichPlayer, typeAttack);
-  }else if(typeAttack == "defenseSpeciale" && player.pokemon.availableSpecialDefense){
+  }else if(typeAttack == "defenseSpeciale" && ! player.pokemon.availableSpecialDefense){
     player.pokemon.availableSpecialDefense = true;
     actionPlayer.score = player.pokemon.scoreSpecialDefense;
+    $("#attaqueSpeciale").addClass("btnDisabled");
+    $("#attaqueNormale").addClass("btnDisabled");
   }
   actionPlayer.type = typeAttack;
   prepareAction(actionPlayer);
+  $("#" + typeAttack).addClass("btnClicked");
 
-}
-
-function actionContinue(){
-  console.log("btn continue clicked");
-  tourBattle.player.status = "OK";
-  $("#infoPlayer")[0].children.statutCombat.innerText = "État : Prêt";
-  $("#infoPlayer #statutCombat").css("color", "green");
-  let message = {};
-  message.type = "infoBattle";
-  message.infoBattle = tourBattle;
-  socket.emit('sendDataToOpponent', message);
 }
 
 function prepareAction(actionPlayer){
   tourBattle.player.action = actionPlayer;
   $("#message")[0].innerText = "Cliquez sur continuer";
   $("#btnContinue").addClass("btnEnabled");
-  $("#attaqueNormale").addClass("btnClicked");
-  $("#attaqueSpeciale").addClass("btnDisabled");
-  $("#defenseSpeciale").addClass("btnDisabled");
+}
+
+function actionContinue(){
+  console.log("btn continue clicked");
+  $("#message")[0].innerText = "En attente du joueur 2 ...";
+  $("#btnContinue").addClass("btnClicked");
+  tourBattle.player.status = "OK";
+  player.status = "OK";
+  $("#infoPlayer")[0].children.statutCombat.innerText = "État : Prêt";
+  $("#infoPlayer #statutCombat").css("color", "green");
+  let message = {};
+  message.type = "infoBattle";
+  message.infoBattle = tourBattle;
+  socket.emit('sendDataToOpponent', message);
+  checkIfBothPlayersAreOk();
+}
+
+
+function checkIfBothPlayersAreOk(){
+  if((player.status == "OK") && (opponent.status == "OK")){
+    console.log("tous les players sont OK");
+    doAttackv2(tourBattle.player.action);
+    resetStateBtnAndPlayer();
+  }else{
+    console.log("encore un player non ok => player : " + player.status + " opponent : " + opponent.status);
+  }
 }
 
 function doAttackv2(actionPlayer){
@@ -86,6 +130,8 @@ function doAttackv2(actionPlayer){
   $(".opponent")[0].children[0].children[0].children.apHP.innerText = opponent.pokemon.hp;
   socket.emit('sendDataToOpponent', message);
 }
+
+
 
 socket.on('sendDataToPlayer', (message) => {
   if(message.type == "action"){
@@ -98,9 +144,12 @@ socket.on('sendDataToPlayer', (message) => {
     console.log("info battle = " + message.infoBattle);
     if(message.infoBattle.player.status == 'OK'){
       console.log("Opponent ready");
-      opponent.pokemon.status = "OK";
+      opponent.status = "OK";
       $("#infoOpponent")[0].children.statutCombat.innerText = "État : Prêt";
       $("#infoOpponent #statutCombat").css("color", "green");
+      checkIfBothPlayersAreOk();
+    }else{
+      opponent.status = "NOK";
     }
   }
 });
